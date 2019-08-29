55e6966aa59e6e876bf5b09c5224e60d279db6d1
lmg1415 <54559013+lmg1415@users.noreply.github.com>
2019-08-29 11:49:54 -0500

    Create hello.c
    Helped set up hello.c

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Author: Cole White
+ * Date: 2019/8/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cole White\n");
+
+  printf("Computer Engineering\n");
+
+  printf("Logan Glynn\n");
+
+  printf("Computer Engineering\n");
+
+  return 0;
+}
